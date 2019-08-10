8cdd50b8376db442d57a63230964d0373fbd5865
Paulin Trognon <paulin.trognon@gmail.com>
2019-08-10 18:35:23 +1200

    remove test text

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,148 +3,18 @@ import './HomePage.css';
 
 export default () => (
   <div className="HomePage-birds">
-    <div className="HomePage-bird">
-      <div className="card">
-        <img
-          className="card-img-top"
-          src="https://i.pinimg.com/originals/9e/4d/6d/9e4d6d2c73d1928703e50d1041b9f8b5.jpg"
-        />
-        <div className="card-body">
-          <h5 className="card-title">Hihi</h5>
-          <p className="card-text">
-            <b>Some quick example text</b> to build on the card title and make
-            up the bulk of the card's content.
-          </p>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
-        </div>
-      </div>
-    </div>
-    <div className="HomePage-bird">
-      <div className="card">
-        <img
-          className="card-img-top"
-          src="https://i.pinimg.com/originals/9e/4d/6d/9e4d6d2c73d1928703e50d1041b9f8b5.jpg"
-        />
-        <div className="card-body">
-          <h5 className="card-title">Tui</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
-        </div>
-      </div>
-    </div>
-    <div className="HomePage-bird">
-      <div className="card">
-        <img
-          className="card-img-top"
-          src="https://i.pinimg.com/originals/9e/4d/6d/9e4d6d2c73d1928703e50d1041b9f8b5.jpg"
-        />
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
-        </div>
-      </div>
-    </div>
-    <div className="HomePage-bird">
-      <div className="card">
-        <img
-          className="card-img-top"
-          src="https://i.pinimg.com/originals/9e/4d/6d/9e4d6d2c73d1928703e50d1041b9f8b5.jpg"
-        />
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
-        </div>
-      </div>
-    </div>
-    <div className="HomePage-bird">
-      <div className="card">
-        <img
-          className="card-img-top"
-          src="https://i.pinimg.com/originals/9e/4d/6d/9e4d6d2c73d1928703e50d1041b9f8b5.jpg"
-        />
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
-        </div>
-      </div>
-    </div>
-    <div className="HomePage-bird">
-      <div className="card">
-        <img
-          className="card-img-top"
-          src="https://i.pinimg.com/originals/9e/4d/6d/9e4d6d2c73d1928703e50d1041b9f8b5.jpg"
-        />
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
-        </div>
-      </div>
-    </div>
-    <div className="HomePage-bird">
-      <div className="card">
-        <img
-          className="card-img-top"
-          src="https://i.pinimg.com/originals/9e/4d/6d/9e4d6d2c73d1928703e50d1041b9f8b5.jpg"
-        />
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
-        </div>
-      </div>
-    </div>
-    <div className="HomePage-bird">
-      <div className="card">
-        <img
-          className="card-img-top"
-          src="https://i.pinimg.com/originals/9e/4d/6d/9e4d6d2c73d1928703e50d1041b9f8b5.jpg"
-        />
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
-        </div>
+    <div className="HomePage-bird card">
+      <img
+        className="HomePage-bird-image card-img-top"
+        src="https://i.pinimg.com/originals/9e/4d/6d/9e4d6d2c73d1928703e50d1041b9f8b5.jpg"
+        alt="Tui"
+      />
+      <div className="card-body">
+        <h5 className="card-title">Tui</h5>
+        <p className="card-text">
+          Le <strong>Tui</strong> blabla bli blabla blou on l&apos;adore il est
+          trop chouette&nbsp;!
+        </p>
       </div>
     </div>
   </div>
